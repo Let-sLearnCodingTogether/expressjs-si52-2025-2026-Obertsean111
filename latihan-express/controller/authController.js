466e71eea025917e20ba7ad49d146ea8e478a372
Obertsean111 <Oberts1310@gmail.com>
2025-10-18 18:47:46 +0700

    tahap 7

--- latihan-express/controller/authController.js
+++ latihan-express/controller/authController.js
@@ -1,18 +1,21 @@
 import UserModel from "../models/userModel.js";
+import { jwtSignUtil } from "../utils/jwtSignUtil.js";
+import {compare, hash} from "../utils/hashUtil.js";
+import { request } from "express";
 
 
 export const register = async (req, res) => {
     try {
-        const registerData = req.body
+        const request = req.body
 
-        console.log(registerData);
+        console.log(request);
 
-        const hashPassword = hash(registerData.password)
+        const hashPassword =await hash(request.password)
 
         await UserModel.create({
-            username : registerData.username,
-            email : registerData.email,
-            password : registerData.password
+            username : request.username,
+            email : request.email,
+            password : hashPassword
         })
 
         res.status(201).json ({
@@ -30,28 +33,30 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
     try {
-        const loginData = req.body
-
-
+        const request = req.body
+        console.log(request);
 
         const user = await UserModel.findOne ({
-            email : loginData.email
+            email : request.email
         })
+        const isPasswordMatch = compare(request.password, user.password);
         //membandingkan user dengan database
-        if (!user) {
+        if (!isPasswordMatch) {
             res.status (404).json ({
                 message : "User tidak di temukan",
                 data : null
             })
         }
+
+
         // membandingkan password dengan database
-        if(user.password == loginData.password) {
+        if(await compare(request.password, user.password)) {
             return res.status(200).json({
                 message : "Login berhasil!",
                 data : {
                     username : user.username,
                     email : user.email,
-                    token : "TOKEN"
+                    token : jwtSignUtil(user)
                 }
             })
         }
